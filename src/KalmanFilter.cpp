12aa107ea62c13e6883008a1dd2b5a28a0d4f091
Ahmed <am.hassan89@gmail.com>
2020-09-20 10:42:34 -0700

    Added initial implementation of Kalman filter class.

---
+++ src/KalmanFilter.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "KalmanFilter.h"
+
+KalmanFilter::KalmanFilter(const Eigen::MatrixXd &A, const Eigen::MatrixXd &B,
+                           const Eigen::MatrixXd &C, const Eigen::MatrixXd &Q,
+                           const Eigen::MatrixXd &R, const Eigen::MatrixXd &P,
+                           double dt) : A(A), B(B), C(C), Q(Q), R(R),
+                           P0(P), sampleTime(dt), nStates(A.rows()),
+                           nInputs(B.cols()), nOutputs(C.rows()),
+                           xHat(nStates), xhatNext(nStates),
+                           I(nStates, nStates)
+{
+    std::cout << "Kalman filter object has been constructed." << std::endl;
+}
+
+void KalmanFilter::init(const Eigen::VectorXd &x0)
+{
+    I.setIdentity();
+    xHat = x0;
+    P = P0;
+    time = 0.0;
+    std::cout << "Kalman filter object has been initialized." << std::endl;
+}
+
+void KalmanFilter::update(const Eigen::VectorXd &y, const Eigen::VectorXd &u)
+{
+    // prediction step
+    xhatNext = A * xHat + B * u;
+    P = A * P * A.transpose() + Q;
+
+    // correction step
+    K = P * C.transpose() * (C * P * C.transpose() + R).inverse();
+    xhatNext += K * (y - C * xhatNext);
+    P =  (I - K * C) * P;
+
+    // prepare for next time step
+    xHat = xhatNext;
+    time += sampleTime;
+}
+
---
+++ src/KalmanFilter.h
@@ -0,0 +1,60 @@
+#ifndef KALMAN_FILTER_H
+#define KALMAN_FILTER_H
+
+#include <Eigen/Dense>
+
+class KalmanFilter
+{
+    public:
+    // constructor
+    KalmanFilter(const Eigen::MatrixXd &A, const Eigen::MatrixXd &B,
+                const Eigen::MatrixXd &C, const Eigen::MatrixXd &Q,
+                const Eigen::MatrixXd &R, const Eigen::MatrixXd &P, double dt);
+
+    // filter initializer
+    void init(const Eigen::VectorXd &x0);
+
+    // filter update function, y: output measurement vector, u: control input vector 
+    void update(const Eigen::VectorXd &y, const Eigen::VectorXd &u);
+
+    // getters
+    Eigen::VectorXd getState()
+    {
+        return xHat;
+    }
+
+    double getTime()
+    {
+        return time;
+    }            
+
+    private:
+    // time and sampleTime
+    double time;
+    double sampleTime;
+
+    // dimensions of the dynamic system
+    int nStates;
+    int nInputs;
+    int nOutputs;
+
+    // dynamic system matrices
+    Eigen::MatrixXd A; // dynamics matrix
+    Eigen::MatrixXd B; // control input matrix
+    Eigen::MatrixXd C; // output matrix
+    Eigen::MatrixXd Q; // process noise covariance matrix
+    Eigen::MatrixXd R; // measurment noise covariance matrix
+
+    // filter matrices
+    Eigen::MatrixXd P; // estimate error covariance matrix
+    Eigen::MatrixXd P0; // initial value for P
+    Eigen::MatrixXd K; // Kalman gain matrix
+    Eigen::MatrixXd I; // identity matrix
+
+    // state estimate at current and next time steps
+    Eigen::VectorXd xHat;
+    Eigen::VectorXd xhatNext;
+};
+
+
+#endif
